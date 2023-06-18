2ddf73c3ccd9723263a536e6cdbc57ff38bc7335
Hadi14 <asusn43@yahoo.com>
2023-06-18 15:41:02 +0330

    ok

--- Config/db.php
+++ Config/db.php
@@ -1,26 +1,12 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Emdad";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT * FROM Hemayat";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "Hmy_CityFamily:" . $row["Hmy_CityFamily"]. " - Hmy_RuralFamily: " . $row["Hmy_RuralFamily"]. " Hmy_MenFamily " . $row["Hmy_MenFamily"]. "<br>";
+<?class Db{
+  public function querypr(){
+    echo "AKKKDDD";
   }
-} else {
-  echo "0 results";
 }
-$conn->close();
-?> 
+
+$db = new Db();
+$db->querypr();
+?>
+
+
+
