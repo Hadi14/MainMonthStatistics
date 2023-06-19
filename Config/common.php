5988fdcf05cf3d3a4bc4f250abbe7ace811fc273
Hadi14 <asusn43@yahoo.com>
2023-06-19 09:02:57 +0330

    ok

--- Config/common.php
+++ Config/common.php
@@ -2,19 +2,16 @@
 function dump($var, $return = false)
 {
     if (is_array($var)) {
-        $out = print_r($var);
-        echo "SSSSSSS" . $out;
+        $out = print_r($var, true);
     } else if (is_object($var)) {
         $out = var_export($var);
-        echo "this is obj";
     } else {
         $out = $var;
-        echo "this is simple var";
     }
-    // if ($return)
-    //     return "\n<pre>$out</pre>\n";
-    // else
-    // echo "<pre>$out</pre>";
+    if ($return)
+        return "\n<pre>$out</pre>\n";
+    else
+        echo "<pre>$out</pre>";
 
     // return
 }
