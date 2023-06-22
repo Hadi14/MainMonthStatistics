0446bff93d1d381578bdbdce67b0f9fc02ed4ed3
Hadi14 <asusn43@yahoo.com>
2023-06-22 17:28:24 +0330

    ok

--- signin.php
+++ signin.php
@@ -42,15 +42,15 @@
       <form class="sign-up-form form" action="" method="get">
         <label class="form-label-wrapper">
           <p class="form-label">نام کاربری</p>
-          <input class="form-input" type="text" placeholder="نام کاربری" required>
+          <input name="uname" class="form-input" type="text" placeholder="نام کاربری" required>
         </label>
         <label class="form-label-wrapper">
           <p class="form-label">رمز عبور</p>
-          <input class="form-input" type="password" placeholder="رمز عبور" required>
+          <input name="pass" class="form-input" type="password" placeholder="رمز عبور" required>
         </label>
         <a class="link-info forget-link" href="##">رمز را فراموش کرده اید؟</a>
         <label class="form-checkbox-wrapper">
-          <input class="form-checkbox" type="checkbox">
+          <input name="remember-me" class="form-checkbox" type="checkbox">
           <span class="form-checkbox-label mx-1">مرا به خاطر بسپار</span>
         </label>
         <button class="form-btn primary-default-btn transparent-btn">ورود</button>
